ba128cadcb02b8bdfd167fd285ac788c908404e6
Kyle Beatty <kylebeatty75@gmail.com>
2016-02-08 16:59:12 -0500

    Fix setup.py to point to (future) run() in main.py
    Now that the setup.py is fixed, all I need to do is put all of
    main.py's code into a run() function.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name = 'minerva',
     version = '1.0',
-    author = 'Kyle Beatty',
-    author_email = 'kylebeatty75@gmail.com',
-    url = 'https://github.com/Nestor15/Minerva',
     description = 'Probability calculations for the board game Risk',
     packages = ['minerva'],
     entry_points = {
-        'console_scripts': 'minerva = minerva.main',
-    }
+        'console_scripts': 'minerva = minerva.main:run',
+    },
+    author = 'Kyle Beatty',
+    author_email = 'kylebeatty75@gmail.com',
+    url = 'https://github.com/Nestor15/Minerva',
 )
